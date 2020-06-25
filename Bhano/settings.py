2fcdb52ee495367f10db4dd540f5c6794c47903d
fidele000 <fihacker000@gmail.com>
2020-06-26 01:54:32 +0300

    updates

--- Bhano/settings.py
+++ Bhano/settings.py
@@ -139,6 +139,7 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 
 STATICFILES_DIRS=[

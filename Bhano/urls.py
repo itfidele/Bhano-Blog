284fede6f239614b8fd7fc1e0e8e144601c7a52b
fidele000 <fihacker000@gmail.com>
2020-06-28 02:00:48 +0300

    updates

--- Bhano/urls.py
+++ Bhano/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from main.views import index,scrape
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,6 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
-    path('',index,name="index"),
+    path('',include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

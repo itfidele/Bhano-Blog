87301190ea3de397e3f547ac71a6a2bb143ff06a
fidele000 <fihacker000@gmail.com>
2020-06-27 20:10:07 +0300

    updates

--- Bhano/settings.py
+++ Bhano/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'static_root'),
 ]
 
 MEDIA_URL='/media/'

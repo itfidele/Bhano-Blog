624ba812e9414f7012aa167442fd5fad28c79775
fidele000 <fihacker000@gmail.com>
2020-06-28 14:56:02 +0300

    updates

--- Bhano/settings.py
+++ Bhano/settings.py
@@ -146,7 +146,7 @@ GRAPPELLI_AUTOCOMPLETE_LIMIT = 5
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),

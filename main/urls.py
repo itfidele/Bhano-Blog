fb3d5164fce3bb8184fa4b3b52658307be7c87b7
fidele000 <fihacker000@gmail.com>
2020-07-01 16:53:49 +0300

    added article category

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # website
     #path('scrape/',scrape,name='scrape'),
     #path('SiteSetup/<str:domain>/<str:name>/',SiteAdd,name='setupsite'),
+    path('<str:post_category>/',ArticleCategory,name='article_category'),
     path('<str:post_category>/<slug:slug>',ArticleDetail,name='article_detail'),
 ]

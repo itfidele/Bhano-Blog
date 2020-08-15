a5b506821a3fcddee5f7a0a32be828c8e0f31309
fidele000 <fihacker000@gmail.com>
2020-08-15 20:54:31 +0300

    updated sitemap

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -2,7 +2,7 @@ from django.contrib.sitemaps import Sitemap
 from .models import Post,Category
 
 class PostSitemap(Sitemap):
-    changefreq = 'always'
+    changefreq = 'daily'
     priority = 0.9
     
     def items(self):
@@ -11,7 +11,7 @@ class PostSitemap(Sitemap):
         return obj.updated
 
 class CategorySitemap(Sitemap):
-    changefreq = 'always'
+    changefreq = 'daily'
     priority = 0.9
     
     def items(self):

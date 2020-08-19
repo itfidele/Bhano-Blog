1eae30a4bada2157630ce04b69d72784d93b1178
fidele000 <fihacker000@gmail.com>
2020-08-19 12:08:40 +0300

    feeds updates

--- main/feeds.py
+++ main/feeds.py
@@ -2,12 +2,12 @@ from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
 from .models import Post
 class LatestPostsFeed(Feed):
-    title = 'Byosehano | Menya Byinshi igihe nyacyo'
+    title = 'Byosehano | Sobanukirwa, Imikino, Ikoranabuhanga, Ubuzima, Politiki, Urukundo, no Hirya no Hino'
     link = '/'
-    description = 'Amakuru mashya kuri byosehano'
+    description = 'Byosehano ni urubuga rukugezaho amakuru atandukanye anjyanye na Sobanukirwa, Imikino, Ikoranabuhanga, Ubuzima, Politiki, Urukundo, no Hirya no Hino nibindi byinshi bitandukanye.'
     def items(self):
-        return Post.published.all()[:5]
+        return Post.published.all().order_by('-publish')
     def item_title(self, item):
         return item.title
     def item_description(self, item):
-        return truncatewords(item.body, 30)
+        return truncatewords(item.body, 500)
